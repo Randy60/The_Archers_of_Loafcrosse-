a2fcfb1cedf39da321ff17602db0e5d028ce46e0
jerrylei98 <jerrylei98@gmail.com>
2016-04-22 09:50:24 -0400

    radius changed can be returned through "circle_hold[n].radius()"

--- bounce.js
+++ bounce.js
@@ -10,6 +10,13 @@ but_1.addEventListener("click", function(){
   circle_hold.push(makeCircle());
 });
 
+var but_2 = document.getElementById("by_size");
+//but_2.addEventListener("click", function(){
+//  circle_hold = circle_hold.map(function(){
+      
+    
+
+
 var makeCircle = function(){
   var board = document.getElementById('board');
   var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
@@ -17,10 +24,12 @@ var makeCircle = function(){
   var xpos = Math.floor((Math.random()*440)) + 30;
   var ypos = Math.floor((Math.random()*440)) + 30;
   var xdir = Math.random()*4-2;
-    var ydir = Math.random()*4-2;
+  var ydir = Math.random()*4-2;
+  var radius = Math.floor((Math.random()*20)) + 20;
   var get_pos = function(){return [xpos, ypos];}
-
-  c.setAttribute("r", 30);
+  var speed = function(){return [xdir, ydir];}
+  var rad = function(){return radius;}
+  c.setAttribute("r", radius);
   c.setAttribute("fill", "red");
   c.setAttribute("stroke", "black");
   c.setAttribute("cx", xpos);
@@ -48,6 +57,8 @@ var makeCircle = function(){
   return {
     update: update,
     get_pos: get_pos,
+    speed: speed,
+    radius: rad,
   };
 }
 
